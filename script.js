2bbe722b76c1dae967420657c47b83ad021f3e6a
Völkel <117395529+SirVoelkel@users.noreply.github.com>
2024-02-01 13:29:20 +0100

    version-2.0

--- script.js
+++ script.js
@@ -24,11 +24,6 @@ search.addEventListener('click', () => {
             return;
         }
 
-        container.style.height = '640px';
-        weatherBox.classList.add('active');
-        weatherDetails.classList.add('active');
-        error404.classList.remove('active');
-
         const image = document.querySelector('.weather-box img');
         const temperature = document.querySelector('.weather-box .temperature');
         const description = document.querySelector('.weather-box .description');
@@ -39,43 +34,51 @@ search.addEventListener('click', () => {
             return;
         } else {
             cityHide.textContent = city;
+
+            container.style.height = '640px';
+            container.classList.add('active');
+            weatherBox.classList.add('active');
+            weatherDetails.classList.add('active');
+            error404.classList.remove('active');
+
+            setTimeout(() => {
+                container.classList.remove('active');
+            }, 2500);
+
+            switch (json.weather[0].main) {
+                case 'Clear':
+                    image.src = 'assets/clear.jpeg';    
+                    break;
+            
+                case 'Rain':
+                    image.src = 'assets/rain.jpeg';    
+                    break;
+                    
+                case 'Snow':
+                    image.src = 'assets/snow.jpeg';    
+                    break;
+    
+                case 'Clouds':
+                    image.src = 'assets/cloud.jpeg';    
+                    break;
+    
+                case 'Mist':
+                    image.src = 'assets/mist.jpeg';    
+                    break;    
+                    
+                case 'Haze':
+                    image.src = 'assets/mist.jpeg';    
+                    break;    
+    
+                default:
+                    image.src = 'assets/cloud.jpeg';
+            }
+    
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+            description.innerHTML = `${json.weather[0].description}`;
+            humidity.innerHTML = `${json.main.humidity}%`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
         }
-
-        switch (json.weather[0].main) {
-            case 'Clear':
-                image.src = 'assets/clear.jpeg';    
-                break;
-        
-            case 'Rain':
-                image.src = 'assets/rain.jpeg';    
-                break;
-                
-            case 'Snow':
-                image.src = 'assets/snow.jpeg';    
-                break;
-
-            case 'Clouds':
-                image.src = 'assets/cloud.jpeg';    
-                break;
-
-            case 'Mist':
-                image.src = 'assets/mist.jpeg';    
-                break;    
-                
-            case 'Haze':
-                image.src = 'assets/mist.jpeg';    
-                break;    
-
-            default:
-                image.src = 'assets/cloud.jpeg';
-        }
-
-        temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
-        description.innerHTML = `${json.weather[0].description}`;
-        humidity.innerHTML = `${json.main.humidity}%`;
-        wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
-
-        
 
     });    
 
